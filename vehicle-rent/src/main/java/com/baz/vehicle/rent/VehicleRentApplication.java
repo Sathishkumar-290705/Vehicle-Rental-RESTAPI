3a037aea3640332d8a7058adb5cad762588cb251
SATHISH KUMAR <sathishkumar290705@gmail.com>
2026-07-12 00:34:43 +0530

    setup
    project setup

---
+++ vehicle-rent/src/main/java/com/baz/vehicle/rent/VehicleRentApplication.java
@@ -0,0 +1,13 @@
+package com.baz.vehicle.rent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRentApplication.class, args);
+	}
+
+}
---
+++ vehicle-rent/src/test/java/com/baz/vehicle/rent/VehicleRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baz.vehicle.rent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
