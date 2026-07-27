b254cfa6a22fa532a778a97e98cf53c40726e540
SATHISH KUMAR <sathishkumar290705@gmail.com>
2026-07-27 15:21:27 +0530

    initial commit
    initial commit

---
+++ vehicle-rent/src/main/java/com/baz/vehicle/rent/repository/AuthRepository.java
@@ -0,0 +1,11 @@
+package com.baz.vehicle.rent.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class AuthRepository{
+//    public interface UserRepository extends JpaRepository<User, Long> {
+//        Optional<User> findByEmail(String email);
+//    }
+}   
